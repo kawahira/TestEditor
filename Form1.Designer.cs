1bc0dea75bf0ec2030c24c6b03cc491dee268dbd
kawahira <kawahirak@gmail.com>
2014-10-15 20:10:45 +0900

    1st commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace WindowsFormsApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 441);
+            this.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer = new Timer();
+        private Image backbuffer;
+        private int mouseX, mouseY;
+        public class Vector
+        {
+            public float x, y, z;
+        }
+        public class Cordinate
+        {
+            public float direction;
+            public Vector pos = new Vector();
+        }
+        public Cordinate position = new Cordinate();
+        public Cordinate target = new Cordinate();
+	    protected override void OnPaint(PaintEventArgs e)
+	    {
+		    base.OnPaint (e);
+            e.Graphics.DrawImage(backbuffer, 0, 0);
+	    }
+
+	    protected override void OnMouseMove(MouseEventArgs e)
+	    {
+		    base.OnMouseMove (e);
+            mouseX = e.X;
+            mouseY = e.Y;
+	    }
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                target.pos.x = mouseX;
+                target.pos.y = mouseY;
+            }
+        }
+        private void Update()
+        {
+        }
+        private void DebugFont()
+        {
+            Graphics g = Graphics.FromImage(backbuffer);
+            SolidBrush brush = new SolidBrush(Color.Black);
+            float hsize = 10 * g.DpiX / 72;
+            g.DrawString("Mouse  X: " + mouseX + " Y:" + mouseY, this.Font, brush, 2, hsize * 0);
+            g.DrawString("Start  X: " + position.pos.x + " Y:" + position.pos.y, this.Font, brush, 2, hsize * 1);
+            g.DrawString("Target X: " + target.pos.x + " Y:" + target.pos.y, this.Font, brush, 2, hsize * 2);
+        }
+        private void callDraw(object sender, EventArgs e)
+        {
+            Graphics g = Graphics.FromImage(backbuffer);
+            {   // clear.
+                Rectangle rect = new Rectangle(0, 0, this.Size.Width, this.Size.Height);
+                Brush brush = new SolidBrush(Color.White);
+                g.FillRectangle(brush, rect);
+            }
+            {   // glid
+                    int gridsize = 16;
+                    int ws = this.Size.Width / gridsize;
+                    int hs = this.Size.Height / gridsize;
+                    for ( int i = 1 ; i < ws ; i ++ )
+                    {
+                        Point sp = new Point((i * gridsize), 0);
+                        Point ep = new Point((i * gridsize), this.Size.Height);
+                        g.DrawLine(Pens.Black, sp, ep);
+                    }
+                    for (int i = 1 ; i < hs; i++)
+                    {
+                        Point sp = new Point(0, (i * gridsize));
+                        Point ep = new Point(this.Size.Width, (i * gridsize));
+                        g.DrawLine(Pens.Black, sp, ep);
+                    }
+            }
+
+            {   // position circles
+                int radius = 20;
+                {   // position
+                    Rectangle rect = new Rectangle((int)position.pos.x - (radius / 2), (int)position.pos.y - (radius / 2), radius, radius);
+                    g.DrawEllipse(Pens.Red, rect);
+                }
+                {   // position
+                    Rectangle rect = new Rectangle((int)target.pos.x - (radius / 2), (int)target.pos.y - (radius / 2), radius, radius);
+                    g.DrawEllipse(Pens.Green, rect);
+                }
+            }
+            Update();
+            DebugFont();
+            Invalidate();
+        }
+
+        public Form1() 
+	    {
+            InitializeComponent();
+
+            SetStyle(
+			    ControlStyles.DoubleBuffer |
+			    ControlStyles.UserPaint |
+			    ControlStyles.AllPaintingInWmPaint, true
+		    );
+
+            backbuffer = new Bitmap(this.Size.Width, this.Size.Height);
+            Graphics g = Graphics.FromImage(backbuffer);
+		    Brush brush = new SolidBrush(Color.White);
+            g.FillRectangle(brush, 0, 0, backbuffer.Width, backbuffer.Height);
+            target.pos.x = position.pos.x = (backbuffer.Width / 2.0f);
+            target.pos.y = position.pos.y = (backbuffer.Height / 2.0f);
+            timer.Tick += new EventHandler(callDraw);
+            timer.Interval = 16;
+            timer.Enabled = true;
+	    }    
+    }
+}
